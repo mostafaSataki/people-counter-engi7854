496ed57f6e2e7da597bc19c8c5546db880d45a7c
justin.adams06@gmail.com <justin.adams06@gmail.com@e9c11d61-48f2-a772-e675-24c0a2c1f766>
2011-07-21 17:12:01 +0000

    added some comments

--- trunk/PeopleCounter/src/main.cpp
+++ trunk/PeopleCounter/src/main.cpp
@@ -22,6 +22,7 @@ int main(){
 	//create a thread
 	unsigned int uiCounter = 0;
 	DWORD qThreadID;
+	// uiCounter is the paramter that can be passed into the thread upon execution if needed
 	HANDLE hThread = CreateThread(0,0,ThreadFn,&uiCounter,0,&qThreadID);
 
 	CvCapture* capture = cvCaptureFromCAM( CV_CAP_ANY );
@@ -48,17 +49,11 @@ int main(){
 	cvDestroyWindow( "People Counter 'Video Feed'" );
 
 	CloseHandle(hThread);
-
-	cout << "done!" ;
 	return 0;
-
 }
 
 DWORD WINAPI ThreadFn(LPVOID vpParam){
-	//	unsigned int& uirCounter = *((unsigned int*)vpParam);
-	//	while(uirCounter < 10000000 ){
-	//		++uirCounter;
-	//	}
+	//anything to be performed in the second thread goes in here
 
 	CvCapture* capture2 = cvCaptureFromCAM( -1 );
 	cvNamedWindow( "People Counter 'Processed Video'");
@@ -80,49 +75,10 @@ DWORD WINAPI ThreadFn(LPVOID vpParam){
 		if ( (cvWaitKey(10) & 255) == 27 ) break;
 	}
 
-	cout << "thread finished!";
 	done = false;
 	cvReleaseCapture( &capture2 );
 	cvDestroyWindow( "People Counter 'Processed Video'" );
 	return 0;
 }
-
-//int main() {
-//	//seclect best available video capture device
-//	CvCapture* capture = cvCaptureFromCAM( CV_CAP_ANY );
-//	if ( !capture ) {
-//		// stop if there is no appropriate device
-//		fprintf( stderr, "ERROR: capture is NULL \n" );
-//		getchar();
-//		return -1;
-//	}
-//
-//	// Create a window to display video
-//	cvNamedWindow( "People Counter 'Video Feed'" );
-//	cvNamedWindow( "People Counter 'Processed Video'");
-//	// Show the image captured from the camera in the window and repeat
-//	while ( 1 ) {
-//		// Get one frame
-//		IplImage* frame=cvQueryFrame( capture );
-//
-//		if ( !frame ) {
-//			//stop is there is no frame captured
-//			fprintf( stderr, "ERROR: frame is null...\n" );
-//			getchar();
-//			break;
-//		}
-//		//display image/frame
-//		cvShowImage( "People Counter 'Video Feed'", frame );
-//		cvShowImage( "People Counter 'Processed Video'", frame);
-//
-//		//If ESC key pressed exit
-//		if ( (cvWaitKey(10) & 255) == 27 ) break;
-//	}
-//	// Release the capture device housekeeping
-//	cvReleaseCapture( &capture );
-//	cvDestroyWindow( "People Counter 'Video Feed'" );
-//	cvDestroyWindow( "People Counter 'Processed Video'" );
-//	return 0;
-//}
 
 
