6bc145b9c6669fe9748c415c0b62c52ab4afd622
justin.sheppard01@gmail.com <justin.sheppard01@gmail.com@e9c11d61-48f2-a772-e675-24c0a2c1f766>
2011-07-23 20:19:23 +0000

    ';,lmnbkvhcgxfz

--- trunk/PeopleCounter/src/main.cpp
+++ trunk/PeopleCounter/src/main.cpp
@@ -25,37 +25,43 @@ using namespace std;
 
 int main(){
 	cvNamedWindow("in", CV_WINDOW_AUTOSIZE);
-	cvNamedWindow("out", CV_WINDOW_AUTOSIZE);
+	//cvNamedWindow("out", CV_WINDOW_AUTOSIZE);
 
-	CvCapture *fc = cvCaptureFromCAM(0);
+	CvCapture *fc = cvCaptureFromCAM(-1);
 
 	IplImage* frame = cvQueryFrame(fc);
-
 	if (!frame) {
 		return 0;
 	}
 
-	IplImage* greyscale = cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 1);
-	IplImage* canny = cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 1);
+	double fps = cvGetCaptureProperty(fc, CV_CAP_PROP_FPS);
+	CvSize size = cvSize((int)cvGetCaptureProperty(fc, CV_CAP_PROP_FRAME_WIDTH), (int)cvGetCaptureProperty(fc, CV_CAP_PROP_FRAME_HEIGHT));
 
-	while(1){
+	CvVideoWriter *writer = cvCreateVideoWriter("video.avi", -1, fps, size);
 
-		frame= cvQueryFrame(fc);
+//	IplImage* greyscale = cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 1);
+//	IplImage* flip = cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 1);
+
+	while((frame = cvQueryFrame(fc)) != NULL){
+
+//		frame= cvQueryFrame(fc);
 		cvShowImage("in", frame);
 
-		// manually convert to greyscale
-		cvCvtColor(frame, greyscale, CV_BGR2GRAY);
-		cvCanny(greyscale, canny, 10, 100, 3);
-		cvShowImage("out", canny);
+		//cvCvtColor(frame, greyscale, CV_BGR2GRAY);
+		//(greyscale, flip);
+		//cvFlip(greyscale, flip, -1);
+		//cvShowImage("out", flip);
 
+		cvWriteFrame(writer, frame);
 		char c = cvWaitKey(33);
 		if (c == 27) {
-			cvReleaseCapture(&fc);
-			cvReleaseImage(&greyscale);
+			cvReleaseVideoWriter(&writer);
+			//cvReleaseImage(&greyscale);
 			cvReleaseImage(&frame);
-			cvReleaseImage(&canny);
+			//cvReleaseImage(&flip);
+			cvReleaseCapture(&fc);
 			cvDestroyWindow("in");
-			cvDestroyWindow("out");
+			//cvDestroyWindow("out");
 			return 0;
 		}
 	}
