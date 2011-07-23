92dac0e7456f23b10763e9f5e8ee9be4b1ef5e3c
justin.sheppard01 <justin.sheppard01@gmail.com>
2011-07-23 02:31:21 +0000

    shaggin around got nothing accomplished

--- PeopleCounter/src/main.cpp
+++ PeopleCounter/src/main.cpp
@@ -14,103 +14,49 @@
 #include "ProcessedVideo.h"
 #include "CamCapture.h"
 #include "FrameHandler.h"
-#include <boost/thread.hpp>
-#include <boost/date_time.hpp>
+#include <string>
+#include <sstream>
+#include <cstring>
+
+
 using namespace cv;
 
 using namespace std;
-FrameHandler *mFrameHandler = new FrameHandler();
-int workerFunc(FrameHandler* fHandler);
+
 int main(){
-	namedWindow("buffer");
-	boost::thread workerThread(workerFunc, mFrameHandler);;
-	cvWaitKey(500);
-	for(;;)
-	{
-		if(mFrameHandler->threadReady){
-			if(mFrameHandler->getFrame().rows <= 0){
-				//cout << "No Frame\n";
-			}else{
-				imshow("buffer", mFrameHandler->getFrame());
-			}
-		}else{
-			std::cout<<"waiting....\n";
-		}
+	cvNamedWindow("in", CV_WINDOW_AUTOSIZE);
+	cvNamedWindow("out", CV_WINDOW_AUTOSIZE);
 
+	CvCapture *fc = cvCaptureFromCAM(0);
 
-		if(((cvWaitKey(1) & 255) == 27)){
-			cvDestroyWindow("edges");
+	IplImage* frame = cvQueryFrame(fc);
+
+	if (!frame) {
+		return 0;
+	}
+
+	IplImage* greyscale = cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 1);
+	IplImage* canny = cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 1);
+
+	while(1){
+
+		frame= cvQueryFrame(fc);
+		cvShowImage("in", frame);
+
+		// manually convert to greyscale
+		cvCvtColor(frame, greyscale, CV_BGR2GRAY);
+		cvCanny(greyscale, canny, 10, 100, 3);
+		cvShowImage("out", canny);
+
+		char c = cvWaitKey(33);
+		if (c == 27) {
+			cvReleaseCapture(&fc);
+			cvReleaseImage(&greyscale);
+			cvReleaseImage(&frame);
+			cvReleaseImage(&canny);
+			cvDestroyWindow("in");
+			cvDestroyWindow("out");
 			return 0;
 		}
 	}
-	return 0;
 }
-
-
-int workerFunc(FrameHandler* fHandler){
-
-
-	VideoCapture cap(0);
-	//Mat edges;
-	namedWindow("edges",1); ; // open the default camera
-	if(!cap.isOpened())  // check if we succeeded
-		return -1;
-
-
-	for(;;)
-	{
-		Mat frame;
-		cap >> frame; // get a new frame from camera
-		if(frame.size <= 0){
-			cout << "NO FRAME!\n" ;
-		}
-		else{
-			fHandler->saveFrame(frame);
-			imshow("edges", frame);
-
-		}
-		if(((cvWaitKey(1) & 255) == 27)){
-			cvDestroyWindow("edges");
-			return 0;
-		}
-	}
-	return 0;
-}
-
-//int main(){
-//	cvNamedWindow("in", CV_WINDOW_AUTOSIZE);
-//	cvNamedWindow("out", CV_WINDOW_AUTOSIZE);
-//
-//	CvCapture *fc = cvCaptureFromCAM(0);
-//
-//	IplImage* frame = cvQueryFrame(fc);
-//
-//	if (!frame) {
-//	    return 0;
-//	}
-//
-//	IplImage* greyscale = cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 1);
-//	IplImage* output = cvCreateImage(cvGetSize(frame),IPL_DEPTH_32F , 1);
-//
-//	while(1){
-//
-//	    frame= cvQueryFrame(fc);
-//	    cvShowImage("in", frame);
-//
-//	    // manually convert to greyscale
-//	    for (int y = 0; y < frame->height; y++) {
-//	        uchar* p = (uchar*) frame->imageData + y* frame->widthStep; // pointer to row
-//	        uchar* gp = (uchar*) greyscale->imageData + y*greyscale->widthStep;
-//	        for(int x = 0; x < frame->width; x++){
-//	            gp[x] = (p[3*x] + p[3*x+1] + p[3*x+2])/3;   // average RGB values
-//	        }
-//	    }
-//
-//	    cvShowImage("out", greyscale);
-//
-//	    char c = cvWaitKey(33);
-//	    if (c == 27) {
-//	        return 0;
-//	    }
-//	}
-//}
